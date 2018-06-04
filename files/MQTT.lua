0a29ac241d7b0f65603e072cd749b4a65e3642b0
bondrogeen <bondrogeen@gmail.com>
2018-06-04 15:05:31 +0300

    ...

--- files/MQTT.lua
+++ files/MQTT.lua
@@ -1,51 +1,50 @@
 local function con(val)
  if val then
-  print("MQTT: trying to connect to "..M.server..":"..M.port)
-  M.mqtt:connect(M.server,M.port,0,function(c)
+  print("MQTT: trying to connect to ".._M.server..":".._M.port)
+  _M.mqtt:connect(_M.server,_M.port,0,function(c)
   print("Connected to MQTT...")
-  M.start=true
-  M.timer:stop()
-  c:subscribe(M.topic.."+/comm/*",0,function(c)print("subscribe success")end)
---  M.pub("comm/in","")
+  _M.start=true
+  _M.recon:stop()
+  c:subscribe(_M.topic.."+/comm/*",0,function(c)print("subscribe success")end)
+  _M.pub("comm/in","")
+  _M.int:start()
  end,
  function(c,r)
   print("failed reason: "..tostring(r))
   collectgarbage()
  end)
 else
- M.mqtt:close()
- M.start=false
+ _M.mqtt:close()
+ _M.start=false
  print("Disconnected to MQTT")
 end
 end
 
 local function pub(t,v)
- if M.start then
-  M.mqtt:publish(M.topic..t,tostring(v),0,0,function(c)end)
+ if _M.start then
+  _M.mqtt:publish(_M.topic..t,tostring(v),0,0,function(c)end)
  end
 end
 
 local function init()
-M.mqtt = mqtt.Client(M.id,120,M.login,M.pass)
-M.pub=pub
-M.timer = tmr.create()
-M.timer:register(5000,tmr.ALARM_AUTO,function(t)
- dofile("MQTT.lua")({con=true})end)
-M.timer:interval(5000)
-M.start=false
-M.mqtt:on("offline",function(client)
-print("offline")
-M.timer:start()
-dofile("MQTT.lua")({con=true})
-end)
-M.mqtt:on("message",dofile("MQTT_mes.lua"))
-M.timer:start()
+_M.mqtt = mqtt.Client(_M.id,120,_M.login,_M.pass)
+_M.pub=pub
+_M.recon=tmr.create()
+_M.recon:register(5000,tmr.ALARM_AUTO,function(t)dofile("MQTT.lua")({con=true})end)
+_M.recon:interval(5000)
+_M.int=tmr.create()
+_M.int:register(5000,tmr.ALARM_AUTO,function(t)_DoT:set() print("timer")end)
+_M.int:interval(_M.timer*1000)
+_M.start=false
+_M.mqtt:on("offline",function(client)print("offline")_M.recon:start()_M.int:stop()dofile("MQTT.lua")({con=true})end)
+_M.mqtt:on("message",dofile("MQTT_mes.lua"))
+_M.recon:start()
 end
 
 return function (t)
  if t.init then init()end
  if t.con then con(true)end
  if t.discon then con(false)end
- if t.status then r=M and M.start end
+ if t.status then r=_M and _M.start end
  return tostring(r)
 end
